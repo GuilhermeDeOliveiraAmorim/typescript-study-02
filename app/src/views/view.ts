74524612f9d953c93acc3112d74061b6fd3d0d14
Guilherme Amorim <guilherme.o.a.ufal@gmail.com>
2022-04-21 11:12:22 -0300

    feat: added function for test runtime performance

--- app/src/views/view.ts
+++ app/src/views/view.ts
@@ -16,12 +16,15 @@ export abstract class View<T> {
     }
 
     public update(model: T): void {
+        const t1 = performance.now();
         let template = this.template(model);
         if (this.escapar) {
             template = template
                 .replace(/<script>[\s\S]*?<\/script>/, '');
         }
         this.elemento.innerHTML = template;
+        const t2 = performance.now();
+        console.log(`template time: ${t2-t1}`);
     }
 
     protected abstract template(model: T): string;
